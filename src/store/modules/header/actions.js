2e1ae116a5602fe5c66353f1240ea3f03cef8824
tjtimer <tjtimer@gmail.com>
2017-06-19 19:14:37 +0200

    updated header actions

--- src/store/modules/header/actions.js
+++ src/store/modules/header/actions.js
@@ -3,25 +3,26 @@ import xducers from 'transducers'
 import * as types from '../../mutation_types'
 import appChannels from '../app/channels'
 
+function watchAppScroll(store) {
+  go(function* () {
+    while (appChannels.scrollChannel !== CLOSED) {
+      let action = yield take(appChannels.scrollChannel)
+      console.log('header watchAppScroll actions: ', action)
+      if (action === 'stop') {
+        yield timeout(3000)
+        store.commit(types.HEADER_HIDE)
+      }
+      if (action === 'up') {
+        console.log('timeout', action)
+        store.commit(types.HEADER_SHOW)
+      }
+    }
+  })
+}
+
 export default {
   initHeader(store) {
     console.log('header started', xducers)
-    store.dispatch('watchAppScroll')
-  },
-  watchAppScroll(store) {
-    go(function* () {
-      while (appChannels.scrollChannel !== CLOSED) {
-        let action = yield take(appChannels.scrollChannel)
-        console.log('header watchAppScroll actions: ', action)
-        if (action === 'stop') {
-          yield timeout(3000)
-          store.commit(types.HEADER_HIDE)
-        }
-        if (action === 'up') {
-          console.log('timeout', action)
-          store.commit(types.HEADER_SHOW)
-        }
-      }
-    })
+    watchAppScroll(store)
   }
 }

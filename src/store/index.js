8401a7467fc536b38611d6e764437d0031e76c0c
tjtimer <tjtimer@gmail.com>
2018-03-09 17:29:13 +0100

    added app module to store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import app from './modules/app'
 import account from './modules/account'
 import concert from './modules/concert'
 import notifications from './modules/notifications'
@@ -5,6 +6,7 @@ import notifications from './modules/notifications'
 
 export default {
   modules: {
+    app,
     account,
     concert,
     notifications
---
+++ src/store/modules/app/mutations.js
@@ -0,0 +1,8 @@
+import * as mt from '../../mutation_types'
+
+export default {
+    [mt.ON_SETUP](state) {
+    },
+    [mt.ON_SHUTDOWN](state) {
+    }
+}
